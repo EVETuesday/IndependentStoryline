333a2dc5fff3f56823ee1cc2a0e8edb7e9fbf4a3
EVE <kirillin970@gmail.com>
2025-03-14 23:12:14 +0300

    0.7 меча

--- src/main/java/com/EveTuPart/Items/LegendarySwordItemUse.java
+++ src/main/java/com/EveTuPart/Items/LegendarySwordItemUse.java
@@ -6,22 +6,12 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 
-public class LegendarySwordItemUse extends Item {
-    public LegendarySwordItemUse(){ super(new Properties().tab(ISConst.modCreativeTab).stacksTo(1));
+public class LegendarySwordItemUse extends SwordItem {
+    public LegendarySwordItemUse(){ super(Tiers.NETHERITE, 1,1,new Properties().tab(ISConst.modCreativeTab));
     }
-    /*@Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if (!level.isClientSide)
-        {
-            player.getInventory().items.set(player.getInventory().selected,new ItemStack(Items.GLOWSTONE_DUST,new Random().nextInt(1,5)));
-        }
-        return super.use(level, player, hand);
-    }*/
     @Override
     public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected){
         if (!pLevel.isClientSide)
@@ -29,7 +19,7 @@ public class LegendarySwordItemUse extends Item {
             Player player = pEntity.level.getNearestPlayer(pEntity,1);
             if (pIsSelected)
             {
-                player.addEffect(new MobEffectInstance(MobEffects.JUMP,9999,5));
+                player.addEffect(new MobEffectInstance(MobEffects.JUMP,5,2));
             }
             else
             {
