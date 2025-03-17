e4e6ba9be98a13027888fdffe73b82a416c13d18
EVETu <114149936+EVETuesday@users.noreply.github.com>
2025-03-17 17:31:29 +0300

    0.7 sword

--- src/main/java/com/is/ISConst.java
+++ src/main/java/com/is/ISConst.java
@@ -1,32 +1,32 @@
 package com.is;
 
-import com.is.data.DelphiForBlocks;
+import com.EveTuPart.Tabs.ModCreativeTab;
+import com.is.server.data.ServerDelphiManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public final class ISConst {
 
-    private ISConst() {}
-
     public static final String MODID = "independent_storyline";
-
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
+    public static final Map<Block, Double> DELPHI_BY_BLOCKS = new HashMap<>() {{
+    }};
 
     public static ResourceLocation rl(String subPath) {
         return ResourceLocation.fromNamespaceAndPath(MODID, subPath);
     }
 
-    public static double getDelphiByBlock(Block block) {
-        for (DelphiForBlocks delphiForBlocks: DelphiForBlocks.values()) {
-            if (delphiForBlocks.acceptFunc.apply(block)) {
-                return delphiForBlocks.income;
-            }
-        }
-        return 0.0d;
+    public static ServerDelphiManager getServerDelphiManager() {
+        return ServerDelphiManager.getInstance();
     }
 
+    public static int getDelphiByBlock(Block block) {
+        throw new NotImplementedException();
+    }
+
+    public static final ModCreativeTab modCreativeTab = new ModCreativeTab();
 }
--- src/main/java/com/is/IndependentStoryline.java
+++ src/main/java/com/is/IndependentStoryline.java
@@ -1,8 +1,6 @@
 package com.is;
 
-import com.is.client.ModKeyBinds;
-import com.is.client.data.ClientAbilityManager;
-import com.is.client.data.ClientDelphiManager;
+import com.EveTuPart.Items.ModItems;
 import com.is.client.event_listeners.ClientEventListener;
 import com.is.client.gui.OverlayOverrides;
 import com.is.server.event_listeners.ServerEventListener;
@@ -26,12 +24,15 @@ public class IndependentStoryline {
     private ServerEventListener.ModEvents modListener;
 
     @SuppressWarnings("removal") // >:D
+    // ;(
     public IndependentStoryline() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         if (Environment.get().getDist().isClient()) new Client(eventBus);
 
         eventBus.register((modListener = new ServerEventListener.ModEvents()));
         MinecraftForge.EVENT_BUS.register((forgeListener = new ServerEventListener()));
+
+
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -52,14 +53,13 @@ public class IndependentStoryline {
 
             modEventBus.register((modListener = new ClientEventListener.ModEvents()));
             MinecraftForge.EVENT_BUS.register((forgeListener = new ClientEventListener()));
+            ModItems.ITEMS.register(modEventBus);
         }
 
         public void commonSetup(FMLCommonSetupEvent event) {
             overlayOverrides = new OverlayOverrides(modEventBus);
-            ClientDelphiManager.initialize();
-            ClientAbilityManager.initialize();
-            ModKeyBinds.init();
         }
+
 
     }
 }
